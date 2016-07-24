e76b25ef56e234363ccf7a209317fb02c6da14d9
nhutuit <tranvannhut4495@gmail.com>
2016-07-24 21:57:04 +0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
 		// new webpack.optimize.OccurrenceOrderPlugin(),
 		// new webpack.HotModuleReplacementPlugin(),
 		// new webpack.NoErrorsPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+		    compress: {
+		        warnings: false
+		    }
+		}),
 		new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
